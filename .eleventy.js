99b8c3f77c49ba6eac461599f20aaead445dc803
Ned Zimmerman <ned@bight.ca>
2019-10-02 18:47:45 -0600

    Save language settings to local storage

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 
     return {
         dir: {
+			dat: "_data",
             input: "src",
             output: "_site"
         }
---
+++ src/_data/languages.js
@@ -0,0 +1,17 @@
+module.exports = {
+	de: 'German',
+	pt: 'Portuguese',
+	hi: 'Hindi',
+	gu: 'Gujarati',
+	sv: 'Swedish',
+	zh: 'Mandarin',
+	af: 'Afrikaans',
+	ml: 'Malayalam',
+	it: 'Italian',
+	es: 'Spanish',
+	ca: 'Catalan',
+	id: 'Indonesian',
+	bn: 'Bengali',
+	fr: 'French',
+	ja: 'Japanese'
+};
