463ad799edadc953ab88d5e7880f78bb73f99cc7
Ned Zimmerman <ned@bight.ca>
2019-10-03 16:47:34 -0600

    Add Service Worker (resolves #3) (#5)
    * Add basic Service Worker.
    * Reorder languages.
    * Return cached resources.
    * Version cache, add offline page.

---
+++ src/service-worker.js
@@ -0,0 +1,53 @@
+/* global addEventListener, caches, clients, fetch, skipWaiting */
+
+const version = '20191003.8';
+const staticCacheName = version + 'staticFiles';
+
+addEventListener('install', installEvent => {
+	skipWaiting();
+	installEvent.waitUntil(
+		caches.open(staticCacheName)
+			.then(staticCache => {
+				return staticCache.addAll([
+					'/css/pwa.css',
+					'/js/pwa.js',
+					'/offline/index.html'
+				]);
+			})
+	);
+});
+
+addEventListener('activate', activateEvent => {
+	activateEvent.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.map(cacheName => {
+					if (cacheName !== staticCacheName) {
+						return caches.delete(cacheName);
+					}
+
+					return true;
+				})
+			);
+		}).then(() => {
+			return clients.claim();
+		})
+	);
+});
+
+addEventListener('fetch', fetchEvent => {
+	const {request} = fetchEvent;
+	fetchEvent.respondWith(
+		caches.match(request)
+			.then(responseFromCache => {
+				if (responseFromCache) {
+					return responseFromCache;
+				}
+
+				return fetch(request)
+					.catch(() => {
+						return caches.match('/offline/index.html');
+					});
+			})
+	);
+});
